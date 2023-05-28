a642f9d78a2dd5da2422dbf481ffba74519bf914
Nguyen Hoang To Nhu <73817808+lacnham@users.noreply.github.com>
2023-05-28 14:58:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    optimizeDeps: {
+      include: ['axios', 'lodash'] 
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      main: path.resolve(__dirname, 'src/index.html') 
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://nom-nom-recipe-web-be.herokuapp.com',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          'Access-Control-Allow-Origin': 'https://nomnomrecipe.netlify.app/',
+          'Access-Control-Allow-Credentials': true,
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }
+      }
+    }
+  }
 })
